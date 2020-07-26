9e574a2d73aa88c3432d147338b49a111c7ef682
visitor83 <wolflouiswang@gmail.com>
2020-07-26 20:39:53 +0800

    week7： 并查集，双端BFS，启发式搜索

---
+++ Week07/1091.二进制矩阵中的最短路径.c
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=1091 lang=c
+ *
+ * [1091] 二进制矩阵中的最短路径
+ */
+
+// @lc code=start
+
+
+int shortestPathBinaryMatrix(int** grid, int gridSize, int* gridColSize){
+
+}
+
+
+// @lc code=end
+
---
+++ Week07/773.滑动谜题.c
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=773 lang=c
+ *
+ * [773] 滑动谜题
+ */
+
+// @lc code=start
+
+
+int slidingPuzzle(int** board, int boardSize, int* boardColSize){
+
+}
+
+
+// @lc code=end
+
